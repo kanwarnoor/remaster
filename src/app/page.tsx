60006465fbbd12b0f15a3a4e04f48780ea5f0fa0
Kanwarnoor Singh <famousfortech500@gmail.com>
2024-12-30 21:47:01 +0530

    addded animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { motion } from "framer-motion";
 export default function Home() {
   const [clicked, setClicked] = useState(false);
   const [todo, setTodo] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
 
   const [task, setTask] = useState<string>("");
 
@@ -14,6 +15,7 @@ export default function Home() {
     try {
       const response = await axios.get("/api/todo");
       setTodo(response.data);
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -32,7 +34,6 @@ export default function Home() {
       await fetchTasks();
       setTask("");
       setClicked(false);
-  
     } catch (error) {
       console.log(error);
     }
@@ -49,25 +50,25 @@ export default function Home() {
 
   const setTrue = async (id: string) => {
     try {
-      await axios.put(`/api/todo/${id}/complete`, {isCompleted: true})
+      await axios.put(`/api/todo/${id}/complete`, { isCompleted: true });
       await fetchTasks();
     } catch (error) {
       console.log(error);
     }
-  }
+  };
   const setFalse = async (id: string) => {
     try {
-      await axios.put(`/api/todo/${id}/complete`, {isCompleted: false})
+      await axios.put(`/api/todo/${id}/complete`, { isCompleted: false });
       await fetchTasks();
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   return !clicked ? (
     <div className="flex flex-col m-auto w-screen h-screen">
       <div className="flex flex-col w-fit m-auto text-center ">
-        <h1 className="text-6xl font-black">Remaster Todo</h1>
+        <h1 className="text-6xl font-bold">Remaster Todo</h1>
         <motion.p
           initial={{
             scale: 1,
@@ -80,76 +81,149 @@ export default function Home() {
         >
           Add +
         </motion.p>
-        {todo.length == 0 && ("No tasks added.")}
-        <ul className="space-y-3 mb-10">
-          {todo.map((task) => {
-            return (
-              <div
-                className="flex w-fit px-5 m-auto flex-row h-12 border-2  rounded-full justify-center items-center"
-                key={task._id}
+
+        {loading ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="xMidYMid"
+            width="50"
+            height="50"
+            className="flex m-auto"
+            style={{
+              shapeRendering: "auto",
+              display: "block",
+              background: "transparent",
+            }}
+          >
+            <g>
+              <circle
+                strokeDasharray="164.93361431346415 56.97787143782138"
+                r="35"
+                strokeWidth="10"
+                stroke="#000000"
+                fill="none"
+                cy="50"
+                cx="50"
               >
-                <li className="text-xl font-bold">{task.title}</li>
-                <div className="ml-5 flex-row flex">
-                  {task.isCompleted ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className="size-7 cursor-pointer"
-                      onClick={() => setFalse(task._id)}
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      onClick={() => setTrue(task._id)}
-                      className="size-7 cursor-pointer"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                      />
-                    </svg>
-                  )}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="red"
-                    className="size-7 cursor-pointer"
-                    onClick={() => handleDelete(task._id)}
+                <animateTransform
+                  keyTimes="0;1"
+                  values="0 50 50;360 50 50"
+                  dur="1s"
+                  repeatCount="indefinite"
+                  type="rotate"
+                  attributeName="transform"
+                ></animateTransform>
+              </circle>
+            </g>
+          </svg>
+        ) : (
+          <div>
+            {todo.length == 0 && "No tasks added."}
+
+            <ul className="space-y-3 mb-10">
+              {todo.map((task, index) => {
+                return (
+                  <motion.div
+                    initial={{
+                      opacity: 0,
+                      y: index * 10,
+                    }}
+                    animate={{
+                      opacity: 1,
+                      y: 0,
+                    }}
+                    className="flex w-fit px-5 m-auto flex-row h-12 border-2  rounded-full justify-center items-center"
+                    key={task._id}
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                    />
-                  </svg>
-                </div>
-              </div>
-            );
-          })}
-        </ul>
+                    <li className="text-xl font-semibold">{task.title}</li>
+                    <div className="ml-5 flex-row flex">
+                      {task.isCompleted ? (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="size-7 cursor-pointer"
+                          onClick={() => setFalse(task._id)}
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      ) : (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          onClick={() => setTrue(task._id)}
+                          className="size-7 cursor-pointer"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                          />
+                        </svg>
+                      )}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="red"
+                        className="size-7 cursor-pointer"
+                        onClick={() => handleDelete(task._id)}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                        />
+                      </svg>
+                    </div>
+                  </motion.div>
+                );
+              })}
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   ) : (
-    <div className="flex flex-col m-auto w-screen h-screen">
-      <div className="flex flex-col w-screen m-auto text-center">
-        <h1 className="text-6xl font-black">Add a Task</h1>
+    <div className="flex flex-col m-auto w-screen h-screen ">
+      <div
+        onClick={() => setClicked(false)}
+        className="absolute right-0 mr-10 mt-10 cursor-pointer hover:scale-110 duration-150 "
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 -960 960 960"
+          className="size-10"
+          fill="black"
+        >
+          <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+        </svg>
+      </div>
+      <motion.div
+        initial={{
+          scale: 0.9,
+          opacity: 0,
+        }}
+        animate={{
+          scale: 1,
+          opacity: 1,
+        }}
+        
+        className="flex flex-col w-fit m-auto text-center"
+      >
+        <h1 className="text-6xl font-bold">Add a Task</h1>
         <form name="flex flex-col" onSubmit={(e) => handleSubmit(e)}>
           <input
-            className="border-2 w-[30%] px-5 py-2 mt-10 rounded-xl text-xl"
+            className="border-2 md:w-[500px] w-[300px] px-5 py-2 mt-10 rounded-xl text-xl font-semibold"
             required
             value={task}
             onChange={(e) => setTask(e.target.value)}
@@ -162,13 +236,13 @@ export default function Home() {
             whileTap={{
               scale: 1.2,
             }}
-            className="flex text-2xl w-fit h-fit font-bold m-auto mb-10 border-2 rounded-full px-5 py-[1px] pt-0 mt-2 border-black bg-black text-white cursor-pointer duration-150"
+            className="flex text-2xl w-fit h-fit font-semibold m-auto mb-10 border-2 rounded-full px-5 py-[1px] pt-0 mt-2 border-black bg-black text-white cursor-pointer duration-150"
             type="submit"
           >
             Save
           </motion.button>
         </form>
-      </div>
+      </motion.div>
     </div>
   );
 }
