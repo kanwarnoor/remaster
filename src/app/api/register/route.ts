010f3ff182a61ca3f07fb951144dd8f01144612e
wellitsnoor <wellitsnoor@gmail.com>
2025-01-04 15:40:17 +0530

    register commented

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,19 +1,19 @@
-import connectDb from "@/libs/connectDb";
-import User from "@/models/User";
-import { NextRequest } from "next/server";
-import bcrypt from "bcrypt";
+// import connectDb from "@/libs/connectDb";
+// import User from "@/models/User";
+// import { NextRequest } from "next/server";
+// import bcrypt from "bcrypt";
 
-export async function POST(request: NextRequest) {
-  const { username, email, password } = await request.json();
+// export async function POST(request: NextRequest) {
+//   const { username, email, password } = await request.json();
 
-  await connectDb();
+//   await connectDb();
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+//   const hashedPassword = await bcrypt.hash(password, 10);
 
-  try {
-    await User.create(username, email, hashedPassword);
-    return { status: 201, body: { message: "User created" } };
-  } catch (err: any) {
-    return { status: 500, body: { message: err.message } };
-  }
-}
+//   try {
+//     await User.create(username, email, hashedPassword);
+//     return { status: 201, body: { message: "User created" } };
+//   } catch (err: any) {
+//     return { status: 500, body: { message: err.message } };
+//   }
+// }
