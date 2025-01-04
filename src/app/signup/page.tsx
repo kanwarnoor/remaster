3683dee7f803af4f6f7e50211a8cf14672968874
wellitsnoor <wellitsnoor@gmail.com>
2025-01-04 15:43:48 +0530

    some changes

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <div className="grid grid-cols-2 max-h-screen">
       <div className="w-full bg-white/50 flex items-center justify-center text-black">
-        <Image src={"/login:signup/sideimage.jpg"} height={0} width={0} sizes="100% 100%" className="w-full h-screen object-cover" alt={""}></Image>
+        <Image src={"/loginSignup/sideimage.jpg"} height={0} width={0} sizes="100% 100%" className="w-full h-screen object-cover" alt={""}></Image>
       </div>
       <Signup />
     </div>
