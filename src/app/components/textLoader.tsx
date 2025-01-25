35521764007aa5e8d140d399382bf4cc1e9cb4f7
wellitsnoor <wellitsnoor@gmail.com>
2025-01-25 21:05:46 +0530

    new ui

--- src/app/components/textLoader.tsx
+++ src/app/components/textLoader.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-export default function Loading() {
+export default function TextLoader() {
   return (
     <>
       <div className="w-fit h-fit fixed top-0 bottom-0 right-0 left-0 m-auto text-center justify-center items-center flex remaster text-remaster -z-10">
