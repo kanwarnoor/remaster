d70f26d7e19877477fe0ca629a2252c124a8dea7
wellitsnoor <wellitsnoor@gmail.com>
2025-03-24 23:47:45 +0530

    styles change

--- src/app/components/Notification.tsx
+++ src/app/components/Notification.tsx
@@ -3,10 +3,19 @@ import { motion } from "framer-motion";
 
 interface NotificationProps {
   message: string;
-  type?: "error" | "success" | "info";
+  type: 'error' | 'success' | 'info' | 'warning';
 }
 
+const notificationStyles = {
+  "error": "bg-remaster text-white",
+  "success": "bg-green-500 text-white",
+  "info": "bg-blue-500 text-white",
+  "warning": "bg-yellow-500 text-black",
+};
+
 export default function Notification({ message, type }: NotificationProps) {
+  const typeStyle = notificationStyles[type] || notificationStyles.info;
+
   return (
     <motion.div
       initial={{
@@ -19,18 +28,11 @@ export default function Notification({ message, type }: NotificationProps) {
       }}
       exit={{
         opacity: 0,
-
       }}
       transition={{
         duration: 0.3,
       }}
-      className={`absolute w-fit h-fit left-0 right-0 bottom-0 m-auto flex mb-10 rounded-full text-black px-5 py-3 overflow-hidden font-bold ${
-        type === "error"
-          ? "bg-remaster"
-          : type === "success"
-          ? "bg-green-500"
-          : "bg-white"
-      }`}
+      className={`absolute w-fit h-fit left-0 right-0 bottom-0 m-auto flex mb-10 rounded-full text-black px-5 py-3 overflow-hidden font-bold ${typeStyle}}`}
     >
       {message}
     </motion.div>
