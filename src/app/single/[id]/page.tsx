17b97a5a8a3071adfcf15cd3f238a429abe1995a
wellitsnoor <wellitsnoor@gmail.com>
2025-05-16 17:14:00 +0530

    test

--- src/app/single/[id]/page.tsx
+++ src/app/single/[id]/page.tsx
@@ -1,6 +1,6 @@
 import SingleTrackClient from "./SingleTrackClient";
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const { id } = await params;
+  const { id } = params;
   return <SingleTrackClient id={id} />;
 }
