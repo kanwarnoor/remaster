ffa14c97861b1af1835c16d8b572d32eaf2ce6f4
wellitsnoor <wellitsnoor@gmail.com>
2025-05-17 15:02:06 +0530

    player icons change

--- src/app/components/Player.tsx
+++ src/app/components/Player.tsx
@@ -48,8 +48,9 @@ export default function Player({
         <div className="flex items-center gap-2">
           <Image
             src={
-              `https://remaster-storage.s3.ap-south-1.amazonaws.com/images/track/${data.track.image}` ||
-              "/music.jpg"
+              data.track.image
+                ? `https://remaster-storage.s3.ap-south-1.amazonaws.com/images/track/${data.track.image}`
+                : "/music.jpg"
             }
             alt={data.track.name}
             width={100}
@@ -70,6 +71,19 @@ export default function Player({
             onClick={() => handleSong("previous")}
           >
             <svg
+              viewBox="0 0 24 24"
+              className="size-9 fill-black"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M5 18L5 6M19 6V18L9 12L19 6Z"
+                stroke="#000000"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            {/* <svg
               viewBox="0 -2 12 12"
               version="1.1"
               xmlns="http://www.w3.org/2000/svg"
@@ -99,7 +113,7 @@ export default function Player({
                   </g>
                 </g>
               </g>
-            </svg>
+            </svg> */}
           </div>
         </div>
         {/* play/pause */}
@@ -111,7 +125,7 @@ export default function Player({
             >
               <svg
                 viewBox="0 0 16 16"
-                fill="white"
+               
                 xmlns="http://www.w3.org/2000/svg"
                 className="size-5 flex fill-black"
               >
@@ -149,35 +163,19 @@ export default function Player({
           {/* next */}
           <div className="cursor-pointer" onClick={() => handleSong("next")}>
             <svg
-              viewBox="0 -2 12 12"
-              version="1.1"
+              viewBox="0 0 24 24"
+              className="size-9 fill-black rotate-180"
               xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              className="fill-black size-9 rotate-180"
             >
-              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                <defs> </defs>
-                <g id="Page-1" stroke="none" strokeWidth="1" fillRule="evenodd">
-                  <g
-                    id="Dribbble-Light-Preview"
-                    transform="translate(-104.000000, -3805.000000)"
-                  >
-                    <g id="icons" transform="translate(56.000000, 160.000000)">
-                      <path
-                        d="M59.9990013,3645.86816 L59.9990013,3652.13116 C59.9990013,3652.84516 58.8540013,3653.25316 58.2180013,3652.82516 L53.9990013,3650.14016 L53.9990013,3652.13116 C53.9990013,3652.84516 53.4260013,3653.25316 52.7900013,3652.82516 L48.4790013,3649.69316 C47.9650013,3649.34616 47.7980013,3648.65316 48.3120013,3648.30616 L52.7900013,3645.17516 C53.4260013,3644.74616 53.9990013,3645.15416 53.9990013,3645.86816 L53.9990013,3647.85916 L58.2180013,3645.17516 C58.8540013,3644.74616 59.9990013,3645.15416 59.9990013,3645.86816"
-                        id="previous-[#999]"
-                      ></path>
-                    </g>
-                  </g>
-                </g>
-              </g>
+              <path
+                d="M5 18L5 6M19 6V18L9 12L19 6Z"
+                stroke="#000000"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
             </svg>
+        
           </div>
         </div>
         {/* progress bar */}
