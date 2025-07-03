7c822d6faca2759dca8e932d0a18013137af65a6
wellitsnoor PC <wellitsnoor@gmail.com>
2025-07-03 22:35:48 +0530

    test

--- src/app/api/album/route.ts
+++ src/app/api/album/route.ts
@@ -25,7 +25,7 @@ export async function GET(req: NextRequest) {
   }
 }
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
   try {
     const { name, description, tracks, image } = await req.json();
 
